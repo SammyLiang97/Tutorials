fdff380a1b0e29978fc329190781ba6195177f52
SammyLiang97 <sammyliang97@gmail.com>
2019-06-30 14:56:47 +0800

    chore(twrt): support eslint to check ts
    Change-Id: I37c6d589a9e2ee7dee32c83848a0010225b6a4e2

---
+++ typescript-webpack-react-tutorial/src/app.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component<{}, {}> {
+    public render() {
+        return (
+            <React.Fragment>
+                Hi
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
